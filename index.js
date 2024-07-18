c39f9644c1a45d1f9164466478414ee57de7ea1d
franklynekirwa2 <sangkirwa2@gmail.com>
2024-07-18 16:32:35 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+ document.addEventListener('DOMContentLoaded',  function(){
+    const change=document.querySelector('#text');
+    if(change){
+    change.textContent="This is really cool!";
+  }else{
+    console.error("Text not found ");
+
+  }
+});
